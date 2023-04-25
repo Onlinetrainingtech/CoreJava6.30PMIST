f20ade112ac674ec5f8209f67b3d0aa0fcffa868
OnlineTraning <57015977+Onlinetrainingtech@users.noreply.github.com>
2023-04-25 19:01:10 +0530

    25-04-2023

---
+++ StringManipulationP1/src/Sample.java
@@ -0,0 +1,16 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		String s1=new String("Welcome");
+		String s2="admin";
+		System.out.println(s1);
+		System.out.println(s1.length());
+		System.out.println(s1.compareTo(s2));
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.toUpperCase());
+
+	}
+
+}
---
+++ StringManipulationP2/src/Demo.java
@@ -0,0 +1,16 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		
+		StringBuilder sb=new StringBuilder("Welcome");
+		System.out.println(sb.append("azar"));
+		System.out.println(sb.indexOf("e"));
+		System.out.println(sb.charAt(0));
+		System.out.println(sb.substring(0,5));
+		
+
+	}
+
+}
